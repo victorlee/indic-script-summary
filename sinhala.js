acdf74751add2bb6dbc0cd570a9ccbfe76915f3e
Victor Lee <63797801+victorlee@users.noreply.github.com>
2021-01-10 17:04:56 +0900

    Create sinhala.js

---
+++ sinhala.js
@@ -0,0 +1,101 @@
+function sinhala() {
+document.getElementById('scriptname').innerHTML = 'Sinhala';
+//consonants
+document.getElementById('switch-ka').innerHTML = 'ක';
+document.getElementById('switch-kha').innerHTML = 'ඛ';
+document.getElementById('switch-ga').innerHTML = 'ග';
+document.getElementById('switch-gha').innerHTML = 'ඝ';
+document.getElementById('switch-ṅa').innerHTML = 'ඞ';
+document.getElementById('switch-ha').innerHTML = 'හ';
+document.getElementById('switch-ca').innerHTML = 'ච';
+document.getElementById('switch-cha').innerHTML = 'ඡ';
+document.getElementById('switch-ja').innerHTML = 'ජ';
+document.getElementById('switch-jha').innerHTML = 'ඣ';
+document.getElementById('switch-ña').innerHTML = 'ඤ';
+document.getElementById('switch-ya').innerHTML = 'ය';
+document.getElementById('switch-śa').innerHTML = 'ශ';
+document.getElementById('switch-ṭa').innerHTML = 'ට';
+document.getElementById('switch-ṭha').innerHTML = 'ඨ';
+document.getElementById('switch-ḍa').innerHTML = 'ඩ';
+document.getElementById('switch-ḍha').innerHTML = 'ඪ';
+document.getElementById('switch-ṇa').innerHTML = 'ණ';
+document.getElementById('switch-ra').innerHTML = 'ර';
+document.getElementById('switch-ṣa').innerHTML = 'ෂ';
+document.getElementById('switch-ta').innerHTML = 'ත';
+document.getElementById('switch-tha').innerHTML = 'ථ';
+document.getElementById('switch-da').innerHTML = 'ද';
+document.getElementById('switch-dha').innerHTML = 'ධ';
+document.getElementById('switch-na').innerHTML = 'න';
+document.getElementById('switch-la').innerHTML = 'ල';
+document.getElementById('switch-sa').innerHTML = 'ස';
+document.getElementById('switch-pa').innerHTML = 'ප';
+document.getElementById('switch-pha').innerHTML = 'ඵ';
+document.getElementById('switch-ba').innerHTML = 'බ';
+document.getElementById('switch-bha').innerHTML = 'භ';
+document.getElementById('switch-ma').innerHTML = 'ම';
+document.getElementById('switch-va').innerHTML = 'ව';
+document.getElementById('ce').innerHTML = `
+`;
+//vowels
+document.getElementById('switch-Xa').innerHTML = 'අ';
+document.getElementById('switch-a').innerHTML = '';
+document.getElementById('a-trans').innerHTML = 'a';
+document.getElementById('switch-Xā').innerHTML = 'ආ';
+document.getElementById('switch-ā').innerHTML = 'ා';
+document.getElementById('ā-trans').innerHTML = 'ā';
+document.getElementById('switch-Xi').innerHTML = 'ඉ';
+document.getElementById('switch-i').innerHTML = 'ි';
+document.getElementById('switch-Xī').innerHTML = 'ඊ';
+document.getElementById('switch-ī').innerHTML = 'ී';
+document.getElementById('switch-Xu').innerHTML = 'උ';
+document.getElementById('switch-u').innerHTML = 'ු';
+document.getElementById('switch-Xū').innerHTML = 'ඌ';
+document.getElementById('switch-ū').innerHTML = 'ූ';
+document.getElementById('switch-Xṛ').innerHTML = 'ඍ';
+document.getElementById('switch-ṛ').innerHTML = 'ෘ';
+document.getElementById('switch-Xṝ').innerHTML = 'ඎ';
+document.getElementById('switch-ṝ').innerHTML = 'ෲ';
+document.getElementById('switch-Xḷ').innerHTML = 'ඏ';
+document.getElementById('switch-ḷ').innerHTML = 'ෟ';
+document.getElementById('switch-Xḹ').innerHTML = 'ඐ';
+document.getElementById('switch-ḹ').innerHTML = 'ෳ';
+document.getElementById('switch-Xe').innerHTML = 'එ';
+document.getElementById('switch-e').innerHTML = 'ෙ';
+document.getElementById('switch-Xai').innerHTML = 'ඓ';
+document.getElementById('switch-ai').innerHTML = 'ෛ';
+document.getElementById('switch-Xo').innerHTML = 'ඔ';
+document.getElementById('switch-o').innerHTML = 'ො';
+document.getElementById('switch-Xau').innerHTML = 'ඖ';
+document.getElementById('switch-au').innerHTML = 'ෞ';
+document.getElementById('ve').innerHTML = `
+`;
+//various signs
+document.getElementById('switch-candrabindu').innerHTML = '';
+document.getElementById('switch-anusvara').innerHTML = 'ං';
+document.getElementById('switch-visarga').innerHTML = 'ඃ';
+document.getElementById('visarga-name').innerHTML = 'visargaya';
+document.getElementById('switch-virama').innerHTML = '්';
+document.getElementById('switch-om').innerHTML = '';
+document.getElementById('switch-danda').innerHTML = '';
+document.getElementById('switch-doubledanda').innerHTML = '';
+document.getElementById('vse').innerHTML = `
+`;
+//numerals
+document.getElementById('switch-0').innerHTML = '';
+document.getElementById('switch-1').innerHTML = '𑇡';
+document.getElementById('switch-2').innerHTML = '𑇢';
+document.getElementById('switch-3').innerHTML = '𑇣';
+document.getElementById('switch-4').innerHTML = '𑇤';
+document.getElementById('switch-5').innerHTML = '𑇥';
+document.getElementById('switch-6').innerHTML = '𑇦';
+document.getElementById('switch-7').innerHTML = '𑇧';
+document.getElementById('switch-8').innerHTML = '𑇨';
+document.getElementById('switch-9').innerHTML = '𑇩';
+document.getElementById('ne').innerHTML = `
+`;
+//link
+document.getElementById('link').innerHTML = `
+full list: <a href="https://www.unicode.org/charts/PDF/U0D80.pdf" target="_blank">https://www.unicode.org/charts/PDF/U0D80.pdf</a>, <a href="https://www.unicode.org/charts/PDF/U111E0.pdf" target="_blank">https://www.unicode.org/charts/PDF/U111E0.pdf</a>
+`;
+//others
+}
